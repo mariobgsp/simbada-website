08f9efd6ce41b9685519599bbeab926a27beec6c
Gio-Farrel <giovanni.farrel.p@gmail.com>
2020-11-26 21:33:17 +0700

    Ubah ke php

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+    require_once 'includes/database.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+            <div class="logo">
+                Water Quality Data
+            </div>
+            <ul class="nav_links">
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>Sensor</li>
+                <li><a href="#"></a>Parameter</li>
+                <li><a href="#"></a>Tentang Kami</li>
+            </ul>
+    </nav>
