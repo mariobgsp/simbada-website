0ead8370a47ff78a7c6ac619317059dfd2cdd75f
mariobgsp <mariobgsp@users.noreply.github.com>
2020-12-23 14:20:14 +0700

    update about us

--- pages/about.php
+++ pages/about.php
@@ -1,9 +1,59 @@
-<?php
-require_once "../includes/header.php"
-?>
-
-
-
-<?php 
-require_once "../includes/footer.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+    <title>About Us</title>
+</head>
+<body class="first-page">
+    <header class="header1">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="../index.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../pages/about.php">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./pages/city-region.php">City/Region</a>
+                </li>
+              </ul>
+            </div>
+        </nav>
+        <div class="container">
+          <div class="content-2">
+            <h1 class="display-5">About Us</h1>
+            <hr class="my-3">
+            <p class="content-text-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius possimus est vel voluptatibus natus rem consectetur asperiores ex non temporibus deleniti quibusdam, cumque obcaecati quia ut assumenda impedit tenetur ipsa corrupti maiores! Recusandae placeat aut quis eos quia aspernatur totam eius optio consequuntur, numquam quibusdam quidem, repellendus commodi rem assumenda, enim odit alias maiores quos voluptatibus doloribus quas in sint minima! Necessitatibus quaerat minus vero sunt earum voluptates illo ducimus, quis eum id quia et aspernatur sequi quam modi voluptatum.
+            </p>
+            <p class="content-text-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam maxime, quas ad officiis at, obcaecati distinctio perspiciatis odio nostrum totam sint sequi reiciendis error quasi hic! Sequi exercitationem autem, qui quis aut nemo voluptatibus quos maiores. Illo iure, nihil optio eum inventore nisi reprehenderit, temporibus consequatur tenetur accusamus qui facilis.
+            </p>
+            <p class="footer-about">
+              Departemen Teknik Nuklir dan Teknik Fisika. Fakultas Teknik
+              Universitas Gadjah Mada 2020
+            </p>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+    </header>
+    
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" ></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</html>
